b3a3d1539e61a14976a0326f4012e51d0bf6b0ab
Hung <rex.hung@jcg-tech.com>
2022-02-18 16:52:09 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
+import logo from './react-helmet.png';
 import './App.css';
+import SEO from './components/SEO';
 
 function App() {
   return (
@@ -7,16 +8,21 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test SEO plugin. <code>React Helmet</code>, check F12 plz.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/nfl/react-helmet"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Helmet
         </a>
+        <SEO title='爬蟲快來爬我' description='爬蟲給你點content' url='http://test.google.com/'>
+          <meta name="description" content='測試 content 1' />
+          <meta name="description" content='測試 content 2' />
+          <meta name="description" content='測試 content 3' />
+        </SEO>
       </header>
     </div>
   );
